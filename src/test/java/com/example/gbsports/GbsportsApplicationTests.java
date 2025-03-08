30556700b1e355b9976f28a13f6ce9e46bb8600d
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2025-03-08 23:16:12 +0700

    nghia

---
+++ src/test/java/com/example/gbsports/GbsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gbsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbsportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
