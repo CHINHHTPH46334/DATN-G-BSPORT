e52b7d54913f40f1815cc48134a787b07eaecd9f
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2025-03-08 22:01:29 +0700

    nghia

--- src/main/java/com/example/gbsports/repository/HoaDonRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.gbsports.repository;
-
-
-import com.example.gbsports.entity.HoaDon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoaDonRepo extends JpaRepository<HoaDon, Integer> {
-}
--- src/main/java/com/example/gbsports/repository/ThuongHieuRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.gbsports.repository;
-
-import com.example.gbsports.entity.ThuongHieu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ThuongHieuRepo extends JpaRepository<ThuongHieu, Integer> {
-}
