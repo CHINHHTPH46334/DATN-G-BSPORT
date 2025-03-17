4372ca28dc0b7745b19453026247748f9fdde57f
CHINHHTPH46334 <chinhhtph46334@fpt.edu.vn>
2025-03-17 16:03:40 +0700

    jj

---
+++ src/main/java/com/example/gbsports/repository/TaiKhoanRepo.java
@@ -0,0 +1,7 @@
+package com.example.gbsports.repository;
+
+import com.example.gbsports.entity.TaiKhoan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaiKhoanRepo extends JpaRepository<TaiKhoan, Integer> {
+}
