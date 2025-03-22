7fdcc9320c0bd8154e4d93f5d013e2afcf1d4cb1
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2025-03-22 20:25:19 +0700

    hong

--- src/main/java/com/example/gbsports/ImportAndExportEx/ExcelExport.java
+++ src/main/java/com/example/gbsports/ImportAndExportEx/ExcelExport.java
@@ -32,18 +32,17 @@ public class ExcelExport {
                 row.createCell(0).setCellValue(chiTiet.getId_chi_tiet_san_pham());
                 row.createCell(1).setCellValue(chiTiet.getTen_san_pham());
                 row.createCell(2).setCellValue(chiTiet.getQr_code());
-                row.createCell(3).setCellValue(chiTiet.getGia_nhap());
-                row.createCell(4).setCellValue(chiTiet.getGia_ban());
-                row.createCell(5).setCellValue(chiTiet.getNgay_tao().toString());
-                row.createCell(6).setCellValue(chiTiet.getNgay_sua().toString());
-                row.createCell(7).setCellValue(chiTiet.getSo_luong());
-                row.createCell(8).setCellValue(chiTiet.getTrang_thai());
-                row.createCell(9).setCellValue(chiTiet.getGia_tri());
-                row.createCell(10).setCellValue(chiTiet.getDon_vi());
-                row.createCell(11).setCellValue(chiTiet.getTen_chat_lieu());
-                row.createCell(12).setCellValue(chiTiet.getTen_danh_muc());
-                row.createCell(13).setCellValue(chiTiet.getTen_thuong_hieu());
-                row.createCell(14).setCellValue(chiTiet.getTen_mau());
+                row.createCell(3).setCellValue(chiTiet.getGia_ban());
+                row.createCell(4).setCellValue(chiTiet.getNgay_tao().toString());
+                row.createCell(5).setCellValue(chiTiet.getNgay_sua().toString());
+                row.createCell(6).setCellValue(chiTiet.getSo_luong());
+                row.createCell(7).setCellValue(chiTiet.getTrang_thai());
+                row.createCell(8).setCellValue(chiTiet.getGia_tri());
+                row.createCell(9).setCellValue(chiTiet.getDon_vi());
+                row.createCell(10).setCellValue(chiTiet.getTen_chat_lieu());
+                row.createCell(11).setCellValue(chiTiet.getTen_danh_muc());
+                row.createCell(12).setCellValue(chiTiet.getTen_thuong_hieu());
+                row.createCell(13).setCellValue(chiTiet.getTen_mau());
             }
 
             ByteArrayOutputStream out = new ByteArrayOutputStream();
