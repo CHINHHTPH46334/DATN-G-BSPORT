64b5d76313ab2c6a7c7fa58c28c9d314c552b795
hawin22 <146153289+hawin22@users.noreply.github.com>
2025-04-01 05:13:44 +0700

    sua

--- src/main/java/com/example/gbsports/response/NhanVienResponse.java
+++ src/main/java/com/example/gbsports/response/NhanVienResponse.java
@@ -8,11 +8,11 @@ public interface NhanVienResponse {
     Integer getIdNhanVien();
     String getMaNhanVien();
     String getTenNhanVien();
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd",timezone = "Asia/Ho_Chi_Minh")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd",timezone = "Asia/Ho_Chi_Minh")
     Date getNgaySinh();
     String getEmail();
     String getDiaChiLienHe();
-    boolean getGioiTinh();
+    Boolean getGioiTinh();
     String getSoDienThoai();
     String getTrangThai();
     String getAnhNhanVien();
