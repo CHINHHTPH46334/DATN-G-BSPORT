1779020ee16290c49e4b61024541bbcf993c3f2e
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2025-04-07 17:43:16 +0700

    gộp code Thu

--- src/main/java/com/example/gbsports/response/HoaDonResponse.java
+++ src/main/java/com/example/gbsports/response/HoaDonResponse.java
@@ -36,9 +36,11 @@ public interface HoaDonResponse {
     LocalDateTime getNgay_chuyen();
     String getGhi_chu();
     String getLoai_hoa_don();
-
     String getMa_san_pham();
     String getTen_san_pham();
     Integer getSo_luong();
+    BigDecimal getGia_ban();
+    String getTrangThaiDonHang();
+    Float getTiLeTrangThaiDonHang();
 
 }
